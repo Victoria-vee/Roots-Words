a838c6f4d8a161e20c56eba9dd2d1008dc9fd4df
Victoria-vee <vickyessien1@gmail.com>
2025-07-30 20:01:26 +0100

    Made a call to action

--- script.js
+++ script.js
@@ -7,14 +7,22 @@ window.addEventListener("scroll", () => {
     } else {
         header.classList.remove("scrolled");
     }
-    
+    if(window,scrollY > 350){
+        header.classList.add("tagline-section");
+    }else{
+        header.classList.remove("tagline-section")
+
+    }    
 });
 
 const close = document.getElementById('close');
+const close2 = document.getElementById('close2');
 const closesignin = document.getElementById('close-signin');
 const signup = document.getElementById('signup');
+const signup2 = document.getElementById('signup2');
 const signin = document.getElementById('signin');
 const signuppopup = document.getElementById('signup-popup');
+const signuppopup2 = document.getElementById('signup-popup2');
 const signinpopup = document.getElementById('signin-popup');
 const signupsubmit = document.getElementById('signupsubmit');
 signup.addEventListener("click",()=>{
@@ -35,4 +43,11 @@ closesignin.onclick=()=>{
 
 signupsubmit.onclick=()=>{
         location.href = "lessons.html"; // Redirect to home page
+}
+signup2.onclick = () => {
+    signuppopup2.style.display = "block";
+}
+
+close2.onclick = () => {
+    signuppopup2.style.display = "none";
 }
